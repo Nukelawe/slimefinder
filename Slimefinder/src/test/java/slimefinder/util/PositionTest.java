efb8ae2d306d865e632561437c6cf54dfc2ad3ce
nukelawe <nukelawe@gmail.com>
2018-07-01 20:49:43 +0300

    testing position movement

--- Slimefinder/src/test/java/slimefinder/util/PositionTest.java
+++ Slimefinder/src/test/java/slimefinder/util/PositionTest.java
@@ -11,6 +11,7 @@ public class PositionTest {
     @Test
     public void testParse() {
         String parsingFailures[] = {
+            "",
             "h2,3",
             "--2,3",
             "-32,31,4",
@@ -30,5 +31,24 @@ public class PositionTest {
         assertEquals(new Position(-1000, 1000), Position.parsePos("-1000,1000"));
         assertEquals(new Position(15, -50), Position.parsePos("\t  15,-50 \t "));
         assertEquals(new Position(3 * 16 + 3, -2 * 16 + 15), Position.parsePos("3:3,-2:15"));
+    }
+
+    @Test
+    public void longMoveSequencesWork() {
+        Position p = new Position(0, 0);
+        int count = 500;
+
+        for (int j = 0; j < count/2; j++) {
+            p.move(1,Direction.SOUTH);
+            for (long i = 0; i < count; i++) {
+                p.move(1,Direction.EAST);
+            }
+            p.move(1,Direction.SOUTH);
+            for (long i = 0; i < count; i++) {
+                p.move(1,Direction.WEST);
+            }
+        }
+
+        assertEquals(new Position(0, count), p);
     }
 }
