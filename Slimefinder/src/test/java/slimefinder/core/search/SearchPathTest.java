e8e98faa520d6498cb16f08bf5e2a36f64a7938e
nukelawe <nukelawe@gmail.com>
2018-07-01 20:52:37 +0300

    fixed overflow bug with path length

--- Slimefinder/src/test/java/slimefinder/core/search/SearchPathTest.java
+++ Slimefinder/src/test/java/slimefinder/core/search/SearchPathTest.java
@@ -15,7 +15,7 @@ import slimefinder.util.Position;
  * @author Matias Ruotsalainen
  */
 public class SearchPathTest {
-    
+
     @Test
     public void positionIsNullBeforeSteps() {
         SearchPath path = new SearchPath(new Position(0, 0), 0, 3);
@@ -142,5 +142,20 @@ public class SearchPathTest {
         assertEquals(path2.getProgress(), path1.getProgress());
         assertEquals(path2.step(), path1.step());
         assertEquals(path2.getPosition(), path2.getPosition());
+    }
+
+
+    @Test
+    public void largePathsFinishNormally() {
+        int maxWidht = 50000;
+        SearchPath path = new SearchPath(new Position(0, 0), 0, maxWidht);
+
+        for (long i = 0; i < (long) maxWidht * (long) maxWidht; i++) {
+            //System.err.println("position=" + path.getPosition() + ", progress=" + path.getProgress());
+            path.step();
+        }
+
+        Position p = path.getPosition();
+        assertEquals(new Position(-(maxWidht / 2 - 1), maxWidht/2), p);
     }
 }
