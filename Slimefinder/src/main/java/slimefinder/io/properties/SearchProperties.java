9184fe72ccb990ac67aa90f7b03e0c821ca1d04a
nukelawe <nukelawe@gmail.com>
2018-07-02 20:00:18 +0300

    corrected max block size default value

--- Slimefinder/src/main/java/slimefinder/io/properties/SearchProperties.java
+++ Slimefinder/src/main/java/slimefinder/io/properties/SearchProperties.java
@@ -34,16 +34,16 @@ public class SearchProperties extends AbstractProperties {
     }
 
     protected void setDefaults() {
-        defaultValues.put(START_POS, new Position(0,0).toString());
         defaultValues.put(APPEND, "" + false);
         defaultValues.put(FINE_SEARCH, "" + false);
         defaultValues.put(RESULTS, "results.csv");
+        defaultValues.put(START_POS, new Position(0,0).toString());
         defaultValues.put(MAX_WIDTH, "" + 1);
         defaultValues.put(MIN_WIDTH, "" + 0);
+        defaultValues.put(MAX_BLOCK_SZ, "" + (2 * Mask.R_CHUNK + 1) * (2 * Mask.R_CHUNK + 1) * 256);
+        defaultValues.put(MIN_BLOCK_SZ, "" + 0);
         defaultValues.put(MAX_CHUNK_SZ, "" + (2 * Mask.R_CHUNK + 1) * (2 * Mask.R_CHUNK + 1));
         defaultValues.put(MIN_CHUNK_SZ, "" + 0);
-        defaultValues.put(MAX_BLOCK_SZ, "" + maxChunkSize * 256);
-        defaultValues.put(MIN_BLOCK_SZ, "" + 0);
     }
 
     @Override
